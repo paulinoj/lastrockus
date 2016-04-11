959e050920ba0f551d8c776f5e07d8a35f125ec9
John Paulino <paulinoj@gmail.com>
2016-04-11 16:20:31 -0700

    remove extraneous text

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import MusicListReducer from './reducer_music_list';
+import Answer from "./reducer_answer";
 
 const rootReducer = combineReducers({
-  musicList: MusicListReducer
+  musicList: MusicListReducer,
+  answer: Answer
 });
 
 export default rootReducer;
