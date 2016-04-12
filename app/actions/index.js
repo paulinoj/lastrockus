b8f82be0b4fdf36837f164f391ec8d0801341da8
John Paulino <paulinoj@gmail.com>
2016-04-11 20:31:57 -0700

    change action types to constants

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,9 +1,12 @@
+export const ANSWER_SUBMITTED = 'ANSWER_SUBMITTED';
+export const GET_NEW_MUSIC_LIST = 'GET_NEW_MUSIC_LIST';
+
 export function submitAnswer(answer) {
   // if user guesses a song correctly make corresponding music player stop playing
   // and calculate points
   console.log('An answer has been submitted:', answer);
   return {
-    type: 'ANSWER_SUBMITTED',
+    type: ANSWER_SUBMITTED,
     payload: answer
   };
 }
@@ -11,7 +14,7 @@ export function submitAnswer(answer) {
 export function getNewMusicList(genre) {
   console.log('New Selection is: ', genre);
   return {
-    type: 'GET_NEW_MUSIC_LIST',
+    type: GET_NEW_MUSIC_LIST,
     payload: genre
   };
 }
--- app/reducers/reducer_answer.js
+++ app/reducers/reducer_answer.js
@@ -1,3 +1,5 @@
+import ANSWER_SUBMITTED from '../actions/index.js';
+
 export default function(state = null, action) {
   switch(action.type) {
     case 'ANSWER_SUBMITTED':
