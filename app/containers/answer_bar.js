f10de45414e0ebc5a21b7f5ced243a1bb2b79c1b
John Paulino <paulinoj@gmail.com>
2016-04-11 19:09:08 -0700

    modify onInputChange function of answer_bar

--- app/containers/answer_bar.js
+++ app/containers/answer_bar.js
@@ -7,26 +7,29 @@ class AnswerBar extends Component {
   constructor(props) {
     super(props);
     this.state = { answer: '' };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({answer: event.target.value});
+    this.props.submitAnswer(event.target.value);
   }
 
   render () {
     return (
       <div className="answer-bar">
         <form className="input-group">
-          <input 
+          <input
+            placeholder="Guess a song"
+            className="form-control" 
             value={this.state.answer}
-            onChange={event => this.onInputChange(event.target.value)} />
+            onChange={this.onInputChange} />
           <span className="input-group-btn">
             <button type="submit" className="btn btn-secondary">Submit</button>
           </span>
         </form>
       </div>
     )
-  }
-
-  onInputChange(answer) {
-    this.setState({answer});
-    this.props.submitAnswer(answer);
   }
 }
 
