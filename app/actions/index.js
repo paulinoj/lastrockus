cb22ed7f89d0a27a88ee80f4c78f16f9c6e89332
John Paulino <paulinoj@gmail.com>
2016-04-18 21:45:00 -0700

    add redux-thunk to modules; add skeleton of signin action creator

--- app/actions/index.js
+++ app/actions/index.js
@@ -48,4 +48,18 @@ export function resetNumberOfMusicPlayersReady() {
   }  
 }
 
+export function signinUser({ email, password }) {
+  return function(dispatch) {
+    // Submit email/password to the server
+
+    // If request is good ...
+    // - Update state to indicate user is authenticated
+    // - Save the JWT token
+    // - redirect to the route '/feature'
+
+    // If request is bad ...
+    // - Show an error to the user
+  }
+}
+
 
--- app/main.js
+++ app/main.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import ReduxThunk from 'redux-thunk';
 import ReduxPromise from 'redux-promise';
 
 import App from './App.js';
 import Signin from './components/auth/signin';
 import reducers from './reducers'
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxThunk, ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
