3766f02631fee02858476c4bae4ebfc04b827dc3
John Paulino <paulinoj@gmail.com>
2016-04-19 22:15:08 -0700

    create welcome component and use as IndexRoute

--- app/main.js
+++ app/main.js
@@ -12,7 +12,7 @@ import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
 import Game from './containers/game';
 import RequireAuth from './components/auth/require_auth';
-
+import Welcome from './components/welcome';
 
 import reducers from './reducers'
 
@@ -22,6 +22,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <IndexRoute component={Welcome} />
         <Route path="signin" component={Signin} />
         <Route path="signout" component={Signout} />
         <Route path="signup" component={Signup} />
