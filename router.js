cae9503234bf6ae1dc4e4bba0fecb80249d1d051
John Paulino <paulinoj@gmail.com>
2016-04-22 03:24:30 -0700

    create passport local strategy and use for signin route

--- router.js
+++ router.js
@@ -5,12 +5,12 @@ const passport = require('passport');
 // create middleware that authenticates and does NOT create session aftewards
 // since this is not cookie-based
 const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
   app.get('/test2', requireAuth, function(req, res) {
     res.send({ hi: 'there'});
   });
-
+  app.post('/test3', requireSignin, Authentication.signin);
   app.post('/test', Authentication.signup);
-
 }
