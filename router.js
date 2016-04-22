2bd19a74ccd873c81dc2081dbecb66b91dc1daa1
John Paulino <paulinoj@gmail.com>
2016-04-22 02:40:02 -0700

    create requireAuth middleware using passport

--- router.js
+++ router.js
@@ -1,6 +1,16 @@
 const Authentication = require('./server/controllers/authentication');
+const passportService = require('./server/services/passport');
+const passport = require('passport');
+
+// create middleware that authenticates and does NOT create session aftewards
+// since this is not cookie-based
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = function(app) {
+  app.get('/test2', requireAuth, function(req, res) {
+    res.send({ hi: 'there'});
+  });
+
   app.post('/test', Authentication.signup);
 
 }
