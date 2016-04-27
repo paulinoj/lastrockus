9760f50e73b967cc51fc69105edcef426d76d218
John Paulino <paulinoj@gmail.com>
2016-04-27 03:26:40 -0700

    start animated logo for welcome page

--- app/components/welcome.js
+++ app/components/welcome.js
@@ -1,3 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { logoDrop } from '../d3_components/logo_drop';
 
-export default () => <div>Welcome to Rockus</div>;
+export default class Visualizer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    let el = ReactDOM.findDOMNode(this);
+    let d3Dispatcher = logoDrop(el)
+  }
+
+  render () {
+    return (
+      <div className="welcome">
+      </div>
+    )
+  }
+}
+
---
+++ app/d3_components/logo_drop.js
@@ -0,0 +1,35 @@
+import d3 from 'd3';
+
+export function logoDrop(el) {
+
+  // initialize the svg canvas
+  var svg = d3.select(el).append("svg")
+      .attr("width", 960)
+      .attr("height", 500)
+      .append("g")
+
+  // the message to animate
+  var msg = "Rockus";
+
+  var ty = svg.append("text")
+      .text(msg);
+      ty.style("font-size", "100px")
+      .style("text-anchor", "end")
+      .attr("transform", "translate(400,0) rotate(180)");
+
+  ty.transition()
+  .attr("transform", "translate(400, 400) rotate(180)")
+  .delay(500)
+  .duration(500)
+  .ease("cubic")
+  .transition()
+    .duration(1000)
+    .ease("bounce")
+    .style('fill', 'darkOrange')
+    .attr("transform", "translate(400, 400) rotate(0)")
+    .transition()
+    .duration(2000)
+    .ease("cubic")
+    .attr("transform", "translate(700, 400) rotate(0)")    
+
+};
