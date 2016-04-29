1162328709498308617ef1be816004e0656b3a95
John Paulino <paulinoj@gmail.com>
2016-04-29 00:23:10 -0700

    update package.json

--- app/containers/header.js
+++ app/containers/header.js
@@ -27,8 +27,8 @@ class Header extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-light">
-        <Link to="/" className="navbar-brand">Redux Auth</Link>
+      <nav className="navbar navbar-default">
+        <Link to="/" className="navbar-brand">Rockus</Link>
         <ul className="nav navbar-nav">
           {this.renderLinks()}
         </ul>
