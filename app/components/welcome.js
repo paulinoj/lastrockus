b9dd453f69dc0b5803d1d20c68b43a3200744db3
John Paulino <paulinoj@gmail.com>
2016-04-30 01:29:32 -0700

    continue refining logo drop and welcome page

--- app/components/welcome.js
+++ app/components/welcome.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { logoDrop } from '../d3_components/logo_drop';
+import styles from '../welcome.css';
 
 export default class Visualizer extends Component {
   constructor(props) {
@@ -12,9 +13,46 @@ export default class Visualizer extends Component {
     let d3Dispatcher = logoDrop(el)
   }
 
+      // <div className="welcome container">
+
+      //       <div className="btn-group btn-group-justified" role="group" aria-label="...">
+      //         <div className="btn-group" role="group">
+      //           <button type="button" className="btn btn-default">
+      //             Sign Up
+      //           </button>
+      //         </div>
+      //       </div>
+
+      //       <div className="btn-group btn-group-justified" role="group" aria-label="...">
+      //         <div className="btn-group" role="group">
+      //           <button type="button" className="btn btn-default">
+      //             Sign Up
+      //           </button>
+      //         </div>
+      //       </div>
+
+
+
+      // </div>
+
+
+
   render () {
+    let button1Classes = `btn btn-info btn-lg ${styles.signin_button}`;
+    let button2Classes = `btn btn-info btn-lg ${styles.login_button}`;
+
     return (
-      <div className="welcome container">
+      <div className="welcome">
+          <div className="text-center">
+            <button type="button" className={button1Classes}>
+              Sign Up
+            </button>
+          </div>
+          <div className="text-center">
+            <button type="button" className={button2Classes}>
+              Login
+            </button>
+          </div>
       </div>
     )
   }
--- app/d3_components/logo_drop.js
+++ app/d3_components/logo_drop.js
@@ -2,34 +2,69 @@ import d3 from 'd3';
 
 export function logoDrop(el) {
 
+  var svg_width = parseInt(d3.select("body").style("width"));
+  let svg_height = 600;
+
   // initialize the svg canvas
-  var svg = d3.select(el).append("svg")
-      .attr("width", 960)
-      .attr("height", 500)
+  var svg = d3.select(el).insert("svg",":first-child")
+      .attr("width", svg_width)
+      .attr("height", svg_height)
       .append("g")
 
   // the message to animate
-  var msg = "Rockus";
+  const msg = "Rockus";
 
-  var ty = svg.append("text")
-      .text(msg);
-      ty.style("font-size", "100px")
-      .style("text-anchor", "end")
-      .attr("transform", "translate(400,-100) rotate(180)");
+  let fontsize = Math.min(150, svg_width/1200 * 150);
+  fontsize = fontsize + "px";
 
-  ty.transition()
-  .attr("transform", "translate(400, 400) rotate(180)")
-  .delay(500)
+  let ty = svg.append("text")
+      .text(msg)
+      .style("font-size", fontsize)
+      .style("font-style", "italic")
+      .style("text-anchor", "end");
+
+  const text_element = svg.select("text");
+  const bbox = text_element.node().getBBox();
+  const text_width = bbox.width;
+  const text_height = bbox.height;
+
+  let translationX1 = (svg_width/2 - text_width/2);
+  let translationX2 = (svg_width/2 + text_width/2);
+  let translationY1 = svg_height - 20 - text_height/2;
+  let translationY2 = svg_height - 20;
+
+  let translation0 = `translate(${translationX1}, -125) rotate(180)`;
+  let translation1 = `translate(${translationX1}, ${translationY1}) rotate(180)`;
+  let translation2 = `translate(${translationX1}, ${translationY2}) rotate(0)`;
+  let translation3 = `translate(${translationX2}, ${translationY2}) rotate(0)`;
+
+  ty.attr("transform", translation0)
+  .transition()
+  .attr("transform", translation1)
+  .delay(1000)
   .duration(500)
   .ease("cubic")
   .transition()
     .duration(1000)
     .ease("bounce")
     .style('fill', 'darkOrange')
-    .attr("transform", "translate(400, 400) rotate(0)")
+    .attr("transform", translation2)
     .transition()
     .duration(2000)
     .ease("cubic")
-    .attr("transform", "translate(700, 400) rotate(0)")    
+    .attr("transform", translation3)    
+
+  function resize() {
+    /* Find the new window dimensions */
+    let svg = d3.select("svg");
+    var svg_width = parseInt(d3.select("body").style("width"));
+    svg.attr("width", svg_width);
+    console.log(svg.attr("width"));
+    let translationX = (svg_width/2 + text_width/2);
+    let translation = "translate(" + translationX + ", " + translationY2 + ") rotate(0)";
+    ty.attr("transform", translation);
+  }
+     
+  d3.select(window).on('resize', resize);
 
 };
