3f46a14b6de86da0d7286a7a6427f9f8a461540f
John Paulino <paulinoj@gmail.com>
2016-05-18 04:39:45 -0700

    delete answer reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import AuthReducer from './reducer_auth';
 import MusicListReducer from './reducer_music_list';
-import Answer from "./reducer_answer";
 import NumberOfMusicPlayersReadyReducer from './reducer_number_of_music_players_ready';
 import musicPlayerOffListReducer from './reducer_music_player_off_list';
 import PlayersActivatedReducer from './reducer_players_activated';
@@ -14,7 +13,6 @@ const rootReducer = combineReducers({
   form: form,
   auth: AuthReducer,
   musicList: MusicListReducer,
-  answer: Answer,
   numberOfMusicPlayersReady: NumberOfMusicPlayersReadyReducer,
   musicPlayerOffList: musicPlayerOffListReducer,
   playersActivated: PlayersActivatedReducer,
