08b404e1522c5c67110008d6c26d7718a378e46b
John Paulino <paulinoj@gmail.com>
2016-07-09 00:59:40 -0700

    clean up d3 code for logo_drop

--- app/d3_components/logo_drop.js
+++ app/d3_components/logo_drop.js
@@ -4,33 +4,112 @@ import soundcloudLogo from '../soundcloud_logo.png';
 
 export function logoDrop(el) {
 
-  let svgWidth = parseInt(d3.select("body").style("width"));
-  let svgHeight = 1200;
+  let svgWidth, svgHeight, buttonGroupTop;
+  let logoFontSize = 106;
+  let blurbFontSize = 30;
+  let textColor = "#FF83E2";
+
+  let button_div = d3.select(el)
+      .append("div")
+      .attr("class", styles.button_group);
 
   // initialize the svg canvas
   var svg = d3.select(el).insert("svg",":first-child")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
-  svg.append("rect")
-    .attr("width", "100%")
-    .attr("height", svgHeight)
-    .attr("fill", "#222");
+  let textGroup = svg.append("g");
+
+  // the message to animate
+  const logo = "Rockus";
+
+  let logoElement = textGroup.append("text")
+    .text(logo)
+    .attr("class", styles.logo)
+    .style("font-size", logoFontSize)
+    .style('fill', textColor)
+    .style("text-anchor", "middle");
+
+  // Temporarily set text of blurbHeadElement to entire blurb in order
+  // to get width of entire blurb.  Second sentence is removed afterwards. 
+
+  let blurbHeadElement = textGroup.append("text")
+    .text("ID 5 songs. Played at the same time.")
+    .style("font-size", blurbFontSize)
+    .style('fill', textColor)
+    .style("text-anchor", "start")
+    .style("opacity", 0);
+
+  const fullBlurbWidth = textWidth(blurbHeadElement) + 10;
+
+  blurbHeadElement.text("ID 5 songs.")
+
+  const blurbHeadWidth = textWidth(blurbHeadElement);
+
+  let blurbEndElement = textGroup.append("text")
+    .text("Played at the same time.")
+    .style("font-size", blurbFontSize)
+    .style('fill', textColor)
+    .style("text-anchor", "end")
+    .style("font-style", "italic")
+    .style("opacity", 0);
+
+  const logoStart = -1200;
+  const logoBottom = 20;
+  const blurbBottom = 80;
+
+  resize();
+
+  blurbHeadElement.attr("transform", translation((fullBlurbWidth - blurbHeadWidth)/2, blurbBottom, 0))
+    .transition()
+    .delay(1000)
+    .duration(3000)
+    .style("opacity", 1)
+    .transition()
+    .duration(3000)
+    .transition()
+    .duration(2000)
+    .attr("transform", translation(0, blurbBottom, 0));
+
+  blurbEndElement.attr("transform", translation(fullBlurbWidth, blurbBottom, 0))
+    .transition()
+    .delay(9000)
+    .duration(2000)
+    .style("opacity", 1);
+
+  logoElement.attr("transform", translation(fullBlurbWidth/2, logoStart, 0))
+    .transition()
+    .delay(16000)
+    .duration(1000)
+    .ease("bounce")
+    .attr("transform", translation(fullBlurbWidth/2, logoBottom, 0))
+    .transition()
+    .duration(3000)
+    .each("end", function() {
+      musicStart();
+      addButtons();
+    });
+
+  d3.select(window).on('resize', resize);
+
+  function textWidth(textElement) {
+    return +textElement.node().getBBox().width;
+  }
+
+  function translation(x, y, rotation) {
+    return `translate(${x}, ${y}) rotate(${rotation})`
+  }
 
   function addButtons() {
-    let buttonGroupTop = (svgHeight/2 + 130) + "px";
     let login_button_classes = `btn btn-info btn-lg ${styles.login_button}`;
     let signup_button_classes = `btn btn-info btn-lg ${styles.signup_button}`;
-    let button_div = d3.select(el)
-      .append("div")
-      .attr("class", styles.button_group)
-      .style("top", buttonGroupTop);
+
     let soundcloudImg = button_div
       .append("div")
       .attr("class", "text-center")
       .append("img")
       .attr("src", soundcloudLogo)
-      .attr("width", 150)
+      .attr("width", 125)
       .style("opacity", 0);
     let login_button = button_div
       .append("div")
@@ -48,20 +127,19 @@ export function logoDrop(el) {
       .text("Sign Up")
       .style("opacity", 0)
       .attr("href", "/signup");
-      soundcloudImg.transition().delay(3000).duration(3000).style("opacity", 1);
-      login_button.transition().delay(3000).duration(3000).style("opacity", 1);
-      signup_button.transition().delay(3000).duration(3000).style("opacity", 1);
+      soundcloudImg.transition().duration(4000).style("opacity", 1);
+      login_button.transition().duration(4000).style("opacity", 1);
+      signup_button.transition().duration(4000).style("opacity", 1);
   }
 
   // MUSICAL NOTES START ****
 
-  setTimeout(musicStart, 19000);
-
   function musicStart() {
     var margin = {top: 0, right: 0, bottom: 0, left: 0};
      
-    var rect = [50,50, svgWidth - 50, svgHeight - 50];
-     
+    // var rect = [50,50, svgWidth - 50, svgHeight - 50];
+    var rect = [50,50, 1900, 1900];
+
     var n =500,
       m = 40,
       padding = 6,
@@ -73,8 +151,8 @@ export function logoDrop(el) {
      
     for (var i in d3.range(n)) {
       nodes.push({radius: 20,
-        // color: color(Math.floor(Math.random() * m)),
-        color: "white",
+        color: color(Math.floor(Math.random() * m)),
+        // color: "white",
         // x: rect[0] + (Math.random() * (rect[2] - rect[0])),
         // y:rect[1] + (Math.random() * (rect[3] - rect[1])),
         x: rect[0] + Math.floor(svgWidth/2),
@@ -86,24 +164,17 @@ export function logoDrop(el) {
       
     var force = d3.layout.force()
       .nodes(nodes)
-      .size([svgWidth, svgHeight])
+      // .size([svgWidth, svgHeight])
+      .size([1200, 1200])
       .gravity(0)
       .charge(0)
-      .on("tick", tick);
-
-    force.start();
-
-    // setTimeout(force.start, 10000);
+      .on("tick", tick)
+      .start();
      
     var notesGroup = svg
-    // .attr("width", svgWidth + margin.left + margin.right)
-    // .attr("height", svgHeight + margin.top + margin.bottom)
-      .insert("g",":nth-child(2)");
-    // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      .insert("g",":nth-child(1)");
      
     var notes = ["\u2669", "\u266A", "\u266B", "\u266C", "\u266D", "\u266E"];
-
-    // var fontSizes = [30, 40, 50];
 
     var fontSizes = [30, 40, 50];
 
@@ -116,7 +187,7 @@ export function logoDrop(el) {
       .attr("x", function(d) { return d.x; })
       .attr("y", function(d) { return d.y; })
       .style("fill", function(d) { return d.color; })
-      .style("opacity", 0.3)
+      .style("opacity", 0.7)
       .call(force.drag);
      
     var flag = false;
@@ -176,130 +247,29 @@ export function logoDrop(el) {
   };
   // *** MUSICAL NOTES END
 
-  function textWidth(textElement) {
-    return +textElement.node().getBBox().width;
-  }
-
-  function textHeight(textElement) {
-    return +textElement.node().getBBox().height;
-  }
-
-  function translation(x, y, rotation) {
-    return `translate(${x}, ${y}) rotate(${rotation})`
-  }
-
-  function xCenterPosition(textWidth, anchor) {
-    svgWidth = parseInt(d3.select("body").style("width"));
-    svg.attr("width", svgWidth);
-
-    if (anchor === "start") {
-      return svgWidth/2 - textWidth/2;
-    }
-    else if (anchor === "end") {
-      return svgWidth/2 + textWidth/2;
-    }
-    else
-    {
-      return svgWidth/2;
-    }
-  }
-
-  let textGroup = svg.append("g");
-
-  // the message to animate
-  const logo = "Rockus";
-
-  let fontsize = Math.min(106, svgWidth/1200 * 150);
-
-  let logoElement = textGroup.append("text")
-    .text(logo)
-    .attr("class", styles.logo)
-    .style("font-size", fontsize)
-    .style('fill', '#FF83E2')
-    .style("text-anchor", "middle");
-
-  let blurbHeadElement = textGroup.append("text")
-    .text("ID 5 songs. Played at the same time.")
-    .style("font-size", "30px")
-    .style('fill', '#FF83E2')
-    .style("text-anchor", "start")
-    .style("opacity", 0);
-
-  const fullBlurbWidth = textWidth(blurbHeadElement) + 10;
-
-  blurbHeadElement.text("ID 5 songs.")
-
-  let blurbEndElement = textGroup.append("text")
-    .text("Played at the same time.")
-    .style("font-size", "30px")
-    .style('fill', '#FF83E2')
-    .style("text-anchor", "end")
-    .style("font-style", "italic")
-    .style("opacity", 0);
-
-  let logoStart = -125;
-  let logoBottom = svgHeight/2 - 50;
-  let blurbBottom = svgHeight/2 + 10;
-  let logoWidth = textWidth(logoElement);
-  const blurbHeadWidth = textWidth(blurbHeadElement);
-
-  blurbHeadElement.attr("transform", translation(xCenterPosition(blurbHeadWidth, "start"), blurbBottom, 0))
-    .transition()
-    .delay(1000)
-    .duration(3000)
-    .style("opacity", 1)
-    .transition()
-    .duration(3000)
-    .transition()
-    .duration(2000)
-    .attrTween("transform", function() {
-      return function(t) {
-        return translation(xCenterPosition(blurbHeadWidth, "start") +
-          ((xCenterPosition(fullBlurbWidth, "start") - xCenterPosition(blurbHeadWidth, "start")) * t),
-          blurbBottom, 0);
-      };
-    });
-
-  blurbEndElement.attr("transform", translation(xCenterPosition(fullBlurbWidth, "end"), blurbBottom, 0))
-    .transition()
-    .delay(9000)
-    .duration(2000)
-    .style("opacity", 1);
-
-  logoElement.attr("transform", translation(xCenterPosition(logoWidth, "middle"), logoStart, 0))
-    .transition()
-    .delay(16000)
-    .duration(1000)
-    .ease("bounce")
-    .attrTween("transform", function() {
-      return function(t) {
-        return translation(xCenterPosition(logoWidth, "middle"), logoStart + (logoBottom - logoStart) * t, 0);
-      };
-    })
-    .each("end", addButtons);
-
-  d3.select(window).on('resize', resize);
-
   function resize() {
     /* Find the new window dimensions */
     let lastSvgWidth = svgWidth;
-    svgWidth = parseInt(d3.select("body").style("width"));
+    let lastSvgHeight = svgHeight;
+
+    svgWidth = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+
+    svgHeight = window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
+
+    buttonGroupTop = (svgHeight/2 + 35) + "px";
+    button_div
+      .style("top", buttonGroupTop);
+
     svg.attr("width", svgWidth);
-    let positionAdjustment = (svgWidth - lastSvgWidth)/2;
+    svg.attr("height", svgHeight);
 
-    // Get current X position of each text element and add the adjustment factor
-    let logoElementX = d3.transform(logoElement.attr("transform")).translate[0] + positionAdjustment;
-    let blurbHeadElementX = d3.transform(blurbHeadElement.attr("transform")).translate[0] + positionAdjustment;
-    let blurbEndElementX = d3.transform(blurbEndElement.attr("transform")).translate[0] + positionAdjustment;
+    let positionAdjustmentY = (svgHeight - lastSvgHeight)/2;
+    let textGroupNewPos = d3.transform(textGroup.attr("transform")).translate[1] + positionAdjustmentY;
 
-    // Get current Y position of each text element
-    let logoElementY = d3.transform(logoElement.attr("transform")).translate[1];
-    let blurbHeadElementY = d3.transform(blurbHeadElement.attr("transform")).translate[1];
-    let blurbEndElementY = d3.transform(blurbEndElement.attr("transform")).translate[1];
-
-    // Translate text elements using newly calculated X and Y positions
-    logoElement.attr("transform", translation(logoElementX, logoElementY, 0));
-    blurbHeadElement.attr("transform", translation(blurbHeadElementX, blurbHeadElementY, 0));
-    blurbEndElement.attr("transform", translation(blurbEndElementX, blurbEndElementY, 0));
+    textGroup.attr("transform", translation((svgWidth - fullBlurbWidth)/2, (svgHeight - 300)/2, 0));
   }
 };
